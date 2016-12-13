40826ed675b37bc36417e3f4ce153dcab145d664
Dan Pitic <dpitic@gmail.com>
2016-12-13 20:41:26 +0800

    View did not work.

--- Work_manager/Work_manager/settings.py
+++ Work_manager/Work_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-0kz78v49wwl^5q+^$(6@)20-$%+2(+5v+zgtoupev$uu!rscr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '192.168.1.56' ]
 
 
 # Application definition
